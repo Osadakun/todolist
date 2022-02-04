db3242e585a2962ca055fee108ef492f6cf7151f
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-02-04 15:55:51 +0900

    ページ下部にボタンになるモックを配置

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -26,20 +26,45 @@ class _TopPageState extends State<TopPage> {
       appBar: AppBar(
         title: Text('Flutter x Firebase for WEB'),
       ),
-      body: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          return CheckboxListTile(
-            controlAffinity: ListTileControlAffinity.leading,
-            title: Text(taskList[index].title),
-            value: taskList[index].isDone,
-            onChanged: (value) {
-              taskList[index].isDone = !taskList[index].isDone;
-              taskList.removeAt(index);
-              setState(() {});
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return CheckboxListTile(
+                controlAffinity: ListTileControlAffinity.leading,
+                title: Text(taskList[index].title),
+                value: taskList[index].isDone,
+                onChanged: (value) {
+                  taskList[index].isDone = !taskList[index].isDone;
+                  taskList.removeAt(index);
+                  setState(() {});
+                },
+              );
             },
-          );
-        },
-        itemCount: taskList.length,
+            itemCount: taskList.length,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 50,
+                  color: Colors.redAccent,
+                  child: Text('未完了タスク', style: TextStyle(color: Colors.white, fontSize: 20)),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text('完了タスク', style: TextStyle(color: Colors.white, fontSize: 20)),
+                  height: 50,
+                  color: Colors.greenAccent,
+                ),
+              ),
+            ],
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {},
