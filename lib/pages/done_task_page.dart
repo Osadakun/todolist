c6d2fd1391d1df43c5f9455b0ddcf561cb3fbdd1
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-02-05 00:31:09 +0900

    タスクの編集したらnull判定されるなぜ

--- lib/pages/done_task_page.dart
+++ lib/pages/done_task_page.dart
@@ -12,6 +12,8 @@ class DoneTaskPage extends StatefulWidget {
 }
 
 class _DoneTaskPageState extends State<DoneTaskPage> {
+  TextEditingController editTitleContoroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -38,7 +40,49 @@ class _DoneTaskPageState extends State<DoneTaskPage> {
                         ListTile(
                           title: Text('編集'),
                           leading: Icon(Icons.edit),
-                          onTap: () {},
+                          onTap: () {
+                            Navigator.pop(context);
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return SimpleDialog(
+                                    title: Container(
+                                      color: Colors.white,
+                                      child: Column(
+                                        children: [
+                                          Text('テキストを編集'),
+                                          Container(
+                                            width: 500,
+                                            child: TextField(
+                                              decoration: InputDecoration(
+                                                  border: OutlineInputBorder()),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.only(top: 30.0),
+                                            child: Container(
+                                              alignment: Alignment.center,
+                                              width: 200,
+                                              height: 30,
+                                              child: ElevatedButton(
+                                                onPressed: () {
+                                                  widget.doneTaskList[index].title = editTitleContoroller.text;
+                                                  widget.doneTaskList[index].updatedTime = DateTime.now();
+                                                  Navigator.pop(context);
+                                                  setState(() {
+                                                  });
+                                                },
+                                                child: Text('編集'),
+                                              ),
+                                            )
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                }
+                            );
+                          },
                         ),
                         ListTile(
                           title: Text('削除'),
--- lib/pages/undone_task_page.dart
+++ lib/pages/undone_task_page.dart
@@ -14,6 +14,8 @@ class UndoneTaskPage extends StatefulWidget {
 }
 
 class _UndoneTaskPageState extends State<UndoneTaskPage> {
+  TextEditingController editTitleContoroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -23,8 +25,7 @@ class _UndoneTaskPageState extends State<UndoneTaskPage> {
           title: Text(widget.undoneTaskList[index].title),
           value: widget.undoneTaskList[index].isDone,
           onChanged: (value) {
-            widget.undoneTaskList[index].isDone =
-                !widget.undoneTaskList[index].isDone;
+            widget.undoneTaskList[index].isDone = !widget.undoneTaskList[index].isDone;
             widget.doneTaskList.add(widget.undoneTaskList[index]);
             widget.undoneTaskList.removeAt(index);
             setState(() {});
@@ -41,7 +42,49 @@ class _UndoneTaskPageState extends State<UndoneTaskPage> {
                         ListTile(
                           title: Text('編集'),
                           leading: Icon(Icons.edit),
-                          onTap: () {},
+                          onTap: () {
+                            Navigator.pop(context);
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return SimpleDialog(
+                                    title: Container(
+                                      color: Colors.white,
+                                      child: Column(
+                                        children: [
+                                          Text('テキストを編集'),
+                                          Container(
+                                            width: 500,
+                                            child: TextField(
+                                              decoration: InputDecoration(
+                                                  border: OutlineInputBorder()),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.only(top: 30.0),
+                                            child: Container(
+                                              alignment: Alignment.center,
+                                              width: 200,
+                                              height: 30,
+                                              child: ElevatedButton(
+                                                onPressed: () {
+                                                  widget.undoneTaskList[index].title = editTitleContoroller.text;
+                                                  widget.undoneTaskList[index].updatedTime = DateTime.now();
+                                                  Navigator.pop(context);
+                                                  setState(() {
+                                                  });
+                                                },
+                                                child: Text('編集'),
+                                              ),
+                                            )
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                }
+                            );
+                          },
                         ),
                         ListTile(
                           title: Text('削除'),
